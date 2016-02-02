52b7dcb576647ea0dde8cc6886b5c42414ae430c
tessakruger <tessarfoto@gmail.com>
2016-02-02 18:58:11 +0000

    13. generating reviews controller and reviews/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 RottenMangoes::Application.routes.draw do
 
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+    end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
