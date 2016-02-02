5e1b035d75a668609071091549e7914513b56823
tessakruger <tessarfoto@gmail.com>
2016-02-02 16:26:44 +0000

    9. adding log in functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)
+	def create
+	  @user = User.new(user_params)
 
-    if @user.save
-      redirect_to movies_path
-    else
-      render :new
-    end
-  end
+	  if @user.save
+	    session[:user_id] = @user.id # auto log in
+	    redirect_to movies_path
+	  else
+	    render :new
+	  end
+	end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 RottenMangoes::Application.routes.draw do
 
-  get "users/new"
-  get "users/create"
   resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
