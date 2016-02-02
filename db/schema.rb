439f201a263648d80eebd7170c595fd381fa2a9e
tessakruger <tessarfoto@gmail.com>
2016-02-02 00:28:50 +0000

    refactoring movies form into partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160201225724) do
+
+  create_table "movies", force: true do |t|
+    t.string   "title"
+    t.string   "director"
+    t.integer  "runtime_in_minutes"
+    t.text     "description"
+    t.string   "poster_image_url"
+    t.datetime "release_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
