c249feb65d2fda5cfde5a7db3e6b64c594f42217
tessakruger <tessarfoto@gmail.com>
2016-02-02 19:46:32 +0000

    16. using some helper methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -22,6 +22,14 @@ class Movie < ActiveRecord::Base
 
   validate :release_date_is_in_the_past
 
+  def review_average
+    if reviews.size > 0
+      reviews.sum(:rating_out_of_ten)/reviews.size
+    else
+      puts "no reviews"
+    end
+  end
+
   protected
 
   def release_date_is_in_the_past
